e0d366e561ae3ddd51f28f1ffe7a248889cd8d85
Benjamin Kindle <benjaminkindle@yahoo.com>
2019-12-08 12:49:34 -0500

    support Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Seam3"
+    name: "Seam3",
+    platforms: [
+        .macOS(SupportedPlatform.MacOSVersion.v10_12),
+        .iOS(SupportedPlatform.IOSVersion.v10)
+    ],
+    products: [
+        .library(
+            name: "Seam3",
+            targets: ["Seam3"]),
+    ],
+    targets: [
+        .target(
+            name: "Seam3",
+            path: "Sources/Classes"
+        )
+    ]
 )
